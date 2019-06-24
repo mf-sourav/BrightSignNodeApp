d4c295d86da9d39993cd7a798fbfe1c2b10bc877
mf-sourav <sourav.chatterjee@mindfiresolutions.com>
2019-06-24 17:16:13 +0530

    added env file for modularity

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var http = require('http');
@@ -5,7 +6,7 @@ app.use(express.static('www'));
 const fs = require('fs');
 var configData = null;
 var bodyParser = require('body-parser');
-var mediaApiUrl = 'https://api-cloud.insteo.com/api/1/AppService.svc/GetAppContentList?type=JSON&'
+var mediaApiUrl = process.env.PLAYER_MEDIA_URL;
 //var vfk = 'c7d93938-a5bf-41';
 //var k = 'c84345d2-146e-4b';
 var vfk = '';
@@ -18,7 +19,7 @@ const request = require('request');
 
 function readConfig() {
   try {
-    rawdata = fs.readFileSync('./www/config.txt');
+    rawdata = fs.readFileSync( process.env.PLAYER_CONFIG_PATH);
     configData = JSON.parse(rawdata);
     vfk = configData.vfk;
     k = configData.k;
@@ -30,7 +31,7 @@ function readConfig() {
 
 function readMediaList() {
   try {
-    rawdata = fs.readFileSync('./www/medialist.txt', 'utf8');
+    rawdata = fs.readFileSync(process.env.PLAYER_MEDIALIST_PATH, 'utf8');
     mediaList = rawdata.split('\n');
     return mediaList;
   } catch (e) {
@@ -38,7 +39,7 @@ function readMediaList() {
   }
 }
 
-app.listen(9090, function () {
+app.listen(process.env.PLAYER_PORT, function () {
   console.log('Example app listening on port 9090!');
   readConfig();
 });
@@ -55,7 +56,7 @@ app.get('/getMedia', function (req, res) {
 
 app.post('/updateConfig', jsonParser, function (req, res) {
   console.log(req.body);
-  fs.writeFileSync('./www/config.txt', JSON.stringify(req.body));
+  fs.writeFileSync(process.env.PLAYER_CONFIG_PATH, JSON.stringify(req.body));
   readConfig();
   res.send(configData);
 });
@@ -90,7 +91,7 @@ function downloadMedia() {
     for (i = 0; i < items.length; i++) {
       console.log(items[i].image);
       fileName = getFileName(items[i].image);
-      download(items[i].image, './www/media/' + fileName, postDownload);
+      download(items[i].image, process.env.PLAYER_MEDIA_PATH + fileName, postDownload);
     }
   });
 }
@@ -104,7 +105,7 @@ function postDownload(file) {
   mediaList = readMediaList();
   if (!mediaList.includes(fileName)) {
     console.log('not present')
-    fs.appendFileSync('./www/medialist.txt', fileName + '\n');
+    fs.appendFileSync(process.env.PLAYER_MEDIALIST_PATH, fileName + '\n');
   } else {
     console.log('exists')
   }
